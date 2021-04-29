e1fbd153132a1ca7a4c054f179f1394f9126f1ec
MarouaneDmy <marouane.demay@gmail.com>
2021-04-29 17:06:20 +0200

    Message d'erreur lorsque le formulaire est invalide

--- modal.js
+++ modal.js
@@ -13,6 +13,25 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const modalClose = document.querySelectorAll(".close");
 const formData = document.querySelectorAll(".formData");
 
+// Messages d'alertes pour validation du formulaire
+modalbg.addEventListener('submit', function (e) {
+  var prenom = document.querySelector("#prenom")
+  var nom = document.querySelector("#nom")
+  var mentions = document.querySelector("#checkbox1")
+  if (prenom.value.length < 2) {
+    alert('Veuillez entrer votre prénom')
+    e.preventDefault()
+  } else if (nom.value.length < 2) {
+    alert('Veuillez entrer votre nom')
+    e.preventDefault()
+  } else if (!mentions.checked) {
+    alert("Vous n'avez pas accepté les CGU")
+    e.preventDefault()
+  } else {
+    alert("Inscription terminée !")
+  }  
+})
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
