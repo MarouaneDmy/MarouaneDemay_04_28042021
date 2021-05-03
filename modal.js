103a289b1d018ec46c574263614f7ecfa2acc7e8
MarouaneDmy <marouane.demay@gmail.com>
2021-05-03 17:00:03 +0200

    Messages d'erreurs sous l'entrée

--- modal.js
+++ modal.js
@@ -15,21 +15,61 @@ const formData = document.querySelectorAll(".formData");
 
 // Messages d'alertes pour validation du formulaire
 modalbg.addEventListener('submit', function (e) {
+  var estValide = true
+
   var prenom = document.querySelector("#prenom")
   var nom = document.querySelector("#nom")
+  var email = document.querySelector("#email")
+  var birthdate = document.querySelector("#birthdate")
+  var quantity = document.querySelector("#quantity")
+  var location = document.querySelectorAll("input[name='location'")
   var mentions = document.querySelector("#checkbox1")
-  if (prenom.value.length < 2) {
-    alert('Veuillez entrer votre prénom')
-    e.preventDefault()
-  } else if (nom.value.length < 2) {
-    alert('Veuillez entrer votre nom')
-    e.preventDefault()
-  } else if (!mentions.checked) {
-    alert("Vous n'avez pas accepté les CGU")
+
+  if (prenom.value.length < 2 || prenom.value.length == 0) {
+    erreur = document.getElementById("errPrenom").textContent = "Veuillez entrer 2 caractères ou plus pour le champ du prénom";
     e.preventDefault()
   } else {
-    alert("Inscription terminée !")
-  }  
+    erreur = document.getElementById("errPrenom").textContent = null;
+  }
+  
+  if (nom.value.length < 2 || nom.value == 0) {
+    erreur = document.getElementById("errNom").textContent = "Veuillez entrer 2 caractères ou plus pour le champ du nom";
+    e.preventDefault()
+  } else {
+    erreur = document.getElementById("errNom").textContent = null;
+  }
+  
+  if (email.value.length < 6) {
+    erreur = document.getElementById("errMail").textContent = "Vous devez entrer votre email";
+    e.preventDefault()
+  } else {
+    erreur = document.getElementById("errMail").textContent = null;
+  }
+  
+  if (birthdate.value.length < 8) {
+    erreur = document.getElementById("errDate").textContent = "Vous devez entrer votre date de naissance.";
+    e.preventDefault()
+  } else {
+    erreur = document.getElementById("errDate").textContent = null;
+  }
+  
+  if (quantity.value.length < 1) {
+    erreur = document.getElementById("errQuantity").textContent = "Veuillez entrer le nombre de tournois auquel vous avez participé";
+    e.preventDefault()
+  } else {
+    erreur = document.getElementById("errQuantity").textContent = null;
+  }
+  
+  if (!mentions.checked) {
+    erreur = document.getElementById("errMentions").textContent = "Vous devez vérifier que vous acceptez les termes et conditions.";
+    e.preventDefault()
+  } else {
+    erreur = document.getElementById("errMentions").textContent = null;
+  }
+  
+  if (!erreur.value) {
+    alert("Merci ! Votre réservation a été reçue.")
+  }   
 })
 
 // launch modal event
