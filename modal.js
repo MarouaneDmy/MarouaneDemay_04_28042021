b842d32ef51dc045457eb7cbfee54ba90160555b
MarouaneDmy <marouane.demay@gmail.com>
2021-05-04 15:04:04 +0200

    Validation bouton radio, message de confirmation

--- modal.js
+++ modal.js
@@ -15,15 +15,19 @@ const formData = document.querySelectorAll(".formData");
 
 // Messages d'alertes pour validation du formulaire
 modalbg.addEventListener('submit', function (e) {
-  var estValide = true
-
   var prenom = document.querySelector("#prenom")
   var nom = document.querySelector("#nom")
   var email = document.querySelector("#email")
   var birthdate = document.querySelector("#birthdate")
   var quantity = document.querySelector("#quantity")
-  var location = document.querySelectorAll("input[name='location'")
+  var location = document.querySelector("input[name='location']:checked")
   var mentions = document.querySelector("#checkbox1")
+
+  function isValid() {
+    if (prenom.value.length >= 2 && nom.value.length >= 2 && email.value.length >= 6 && birthdate.value.length >= 8 && location != null && quantity.value.length >= 1 && mentions.checked){
+      alert("Merci ! Votre réservation a été reçue.")
+    }
+  }
 
   if (prenom.value.length < 2 || prenom.value.length == 0) {
     erreur = document.getElementById("errPrenom").textContent = "Veuillez entrer 2 caractères ou plus pour le champ du prénom";
@@ -52,12 +56,19 @@ modalbg.addEventListener('submit', function (e) {
   } else {
     erreur = document.getElementById("errDate").textContent = null;
   }
+
+  if (location != null) {
+    erreur = document.getElementById("errLocation").textContent = null;
+  } else {
+    erreur = document.getElementById("errLocation").textContent = "Vous devez valider une option.";
+    e.preventDefault()
+  }
   
   if (quantity.value.length < 1) {
     erreur = document.getElementById("errQuantity").textContent = "Veuillez entrer le nombre de tournois auquel vous avez participé";
     e.preventDefault()
   } else {
-    erreur = document.getElementById("errQuantity").textContent = null;
+    erreur = document.getElementById("errQuantity").textContent = null;   
   }
   
   if (!mentions.checked) {
@@ -67,9 +78,8 @@ modalbg.addEventListener('submit', function (e) {
     erreur = document.getElementById("errMentions").textContent = null;
   }
   
-  if (!erreur.value) {
-    alert("Merci ! Votre réservation a été reçue.")
-  }   
+  isValid()
+    
 })
 
 // launch modal event
